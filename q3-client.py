e0f88432d607a89e596648f38638579d6f1ce472
iliyona <iliyaaziz10@gmail.com>
2021-12-24 07:50:11 +0000

    adding code

---
+++ q3-client.py
@@ -0,0 +1,58 @@
+import socket
+
+
+ClientSocket = socket.socket()
+host = '192.168.56.6'
+port = 8888
+
+print('[+] Waiting for connection')
+try:
+	ClientSocket.connect((host, port))
+except socket.error as e:
+	print(str(e))
+
+Response =ClientSocket.recv(1024)
+print(Response)
+
+print(r"""
+-----------------------------------------
+ __________
+| ________ |   
+||12345678||
+|----------|     _____      _            _       _
+|[M|#|C][-]|    / ____|    | |          | |     | |
+|[7|8|9][+]|   | |     __ _| | ___ _   _| | __ _| |_ ___  _ __
+|[4|5|6][x]|   | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__|
+|[1|2|3][%]|   | |___| (_| | | (__| |_| | | (_| | || (_) | |
+|[.|O|:][=]|    \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+|----------|
+	     Client Version
+-----------------------------------------
+               """)
+
+while True:
+
+	status = 0
+	while(status == 0):
+		option = input('[+] Enter type of operation (log | sqrt | exp): ')
+		if(option == 'log'):
+			value1 = input("[+] Enter the value: ")
+			status = 1
+		elif(option == 'sqrt'):
+			value1 = input("[+] Enter the value: ")
+			status = 1
+		elif(option == 'exp'):
+			value1 = input("[+] Enter the value: ")
+			status = 1
+		else:
+			print("[+] Invalid operation, please re-enter..")
+			status = 0
+
+
+	Input = option + " " +  value1
+	ClientSocket.send(str.encode(Input))
+	Response = ClientSocket.recv(1024)
+	print(Response.decode('utf-8'))
+	print("\n")
+
+ClientSocket.close()
---
+++ q3-server.py
@@ -0,0 +1,112 @@
+import socket
+import sys
+import time
+import errno
+import math
+from multiprocessing import Process
+
+
+print(r"""
+-----------------------------------------
+ __________
+| ________ |    
+||12345678||	
+|----------|     _____      _            _       _
+|[M|#|C][-]|    / ____|    | |          | |     | |
+|[7|8|9][+]|   | |     __ _| | ___ _   _| | __ _| |_ ___  _ __
+|[4|5|6][x]|   | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__|
+|[1|2|3][%]|   | |___| (_| | | (__| |_| | | (_| | || (_) | |
+|[.|O|:][=]|    \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+|----------|
+	     Server Version
+-----------------------------------------
+               """)
+
+def calcLog(x):
+	print(f"[+] Calculating log({x})")
+	x = int(x)
+	try:
+		answer = math.log(x)
+	except:
+		answer = "unable to be calculated. Please retry again.."
+
+	return answer
+
+def calcSqrt(x):
+	print(f"[+] Calculating square root of {x}")
+	x = int(x)
+	if(x >= 0):
+		try:
+			answer = math.sqrt(x)
+		except:
+			answer = "unable to be calculated. Please retry again.."
+	else:
+		answer = "unable to be calculated because it is a negative value."
+
+	return answer
+
+def calcExp(x):
+	print(f"[+] Calculating exponential of {x}")
+	x = float(x)
+	try:
+		answer = math.exp(x)
+	except:
+		answer = "unable to be calculated. Please retry again.."
+
+	return answer
+
+
+def process_start(s_sock):
+	s_sock.send(str.encode('[+] Welcome to the Server\n'))
+	while True:
+		data = s_sock.recv(2048)
+		#print(data)
+		data = data.decode('utf-8')
+		try:
+			option, value1 = data.split(" ", 2)
+		except:
+			print("[+] Unable to calculate, client disconnected\n")
+		#print(option, value1, value2)
+		#data = data.decode('utf-8')
+		#print(data)
+		if not data:
+			break
+		#if(data == 'a'):
+		#	print("Hello World")
+
+		if(option == 'log'):
+			answer = calcLog(value1)
+		elif(option == 'sqrt'):
+			answer = calcSqrt(value1)
+		elif(option == 'exp'):
+			answer = calcExp(value1)
+
+		message = "[+] The answer is  %s." % str(answer)
+		print("[+] Completed\n------------------------------------\n")
+		s_sock.sendall(str.encode(message))
+
+		#s_sock.sendall(str.encode(ok_message))
+	s_sock.close()
+
+if __name__ == '__main__':
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("", 8888))
+	print("[+] Listening for input from clients...")
+	s.listen(3)
+	try:
+		while True:
+			try:
+				s_sock, s_addr = s.accept()
+				p = Process(target = process_start, args = (s_sock,))
+				p.start()
+
+			except socket.error:
+				print('[+] Got a socket error')
+
+	except Exception as e:
+		print('[+] An exception occured!')
+		print(e) 
+		sys.exit(1)
+
+	finally:
+		s.close()
